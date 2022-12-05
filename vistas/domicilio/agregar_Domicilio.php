40cacb644b604fe90a2af1c1dac8b61c203cf48c
Choskua <elyoskua@gmail.com>
2022-12-05 03:37:54 -0700

    Domicilio cambiado

--- vistas/domicilio/agregar_Domicilio.php
+++ vistas/domicilio/agregar_Domicilio.php
@@ -44,25 +44,26 @@
                     <div class="form-group">
                         <label class="control-label col-md-3">Nombre del Cliente</label>
                         <div class="col-md-8">
-                            <input class="form-control" type="text" placeholder="Nombre del cliente" disabled>
+                            <input class="form-control" type="text" placeholder="Nombre del cliente" pattern="[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ. ]{3,15}" value="<?$tallerSQL->nombreCliente?>" disabled>
                         </div>
                     </div>
                     <div class="form-group">
                         <label class="control-label col-md-3">Telefono del Cliente</label>
                         <div class="col-md-8">
-                            <input class="form-control" type="text" placeholder="Telefono del cliente" disabled>
+                            <input class="form-control" type="text" placeholder="Telefono del cliente" pattern="[0-9]{10,13}" value="<?$tallerSQL->telefono?>" disabled>
                         </div>
                     </div>
                     <div class="form-group">
                         <label class="control-label col-md-3">Correo del Cliente</label>
                         <div class="col-md-8">
-                            <input class="form-control" type="text" placeholder="Correo del cliente" disabled>
+                            <input class="form-control" type="text" 
+                            pattern="^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$" placeholder="Correo del cliente" disabled>
                         </div>
                     </div>
                     <div class="form-group">
                         <label class="control-label col-md-3">Direccion del Cliente</label>
                         <div class="col-md-8">
-                            <input class="form-control" type="text" placeholder="Direccion del cliente" disabled>
+                            <input class="form-control" type="text" placeholder="Direccion del cliente" pattern="[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ. ]{15,30}" disabled>
                         </div>
                     </div>                    
                     <div class="col-lg-10">
@@ -73,31 +74,35 @@
                   <div class="form-group">
                     <label class="control-label col-md-3" for="Problematica">Problematica para el servicio</label>
                     <div class="col-md-8">
-                      <textarea class="form-control" name="problematica" rows="4" placeholder="Problematica para el servicio" required=""></textarea>
+                      <textarea class="form-control" name="problematica" rows="4" placeholder="Problematica para el servicio" pattern="[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ. ]{7,100}" required=""></textarea>
                     </div>
                   </div>
 
                   <div class="form-group">
                         <div class="col-lg-10">
-                        <input class="form-control" name="observaciones" type="hidden">
+                        <input class="form-control" name="observaciones" pattern="[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ. ]{7,100}" type="hidden">
                       </div>
                     </div>
 
                     <div class="form-group">
                         <div class="col-lg-10">
-                        <input class="form-control" name="presupuesto" type="hidden">
+                        <input class="form-control" name="presupuesto" pattern="[0-9.]{1,12}" type="hidden">
                       </div>
                     </div>
                   
                     <div class="form-group">
                         <div class="col-lg-10">
-                        <input class="form-control" name="costoTotal" type="hidden">
+                        <input class="form-control" name="costoTotal" pattern="[0-9.]{1,12}" type="hidden">
                       </div>
                     </div>
-
+                    <?php
+                        date_default_timezone_set('America/Mazatlan');
+                        $fecha_actual=date("Y-m-d");
+                        $hora_actual=date("H:i:S");
+                        ?>
                     <div class="form-group">
                         <div class="col-lg-10">
-                        <input class="form-control" name="horaInicio" type="hidden">
+                        <input class="form-control" name="horaInicio" type="hidden" placeholder= "Hora" value="<?= $hora_actual?>">
                       </div>
                     </div>
                   
@@ -109,14 +114,16 @@
 
                     <div class="form-group">
                         <div class="col-lg-10">
-                        <input class="form-control" name="horasRealizadas" type="hidden">
+                        <input class="form-control" name="horasRealizadas" type="hidden" placeholder= "horas realizadas" pattern="[0-9.]{1,12}">
                       </div>
                     </div>
 
                     <div class="form-group">
                         <label class="control-label col-md-3" for="FechaProgramada">Fecha solicitud de servicio</label>
                         <div class="col-md-8">
-                            <input class="form-control" name="fechaProgramada" type="text" placeholder="Fecha de solicitud de servicio" required="">
+                            <input class="form-control" name="fechaProgramada" type="text" placeholder="Fecha de solicitud de servicio" 
+                            pattern="(19|20)(((([02468][048])|([13579][26]))-02-29)|(\d{2})-((02-((0[1-9])|1\d|2[0-8]))|((((0[13456789])|1[012]))-((0[1-9])|((1|2)\d)|30))|(((0[13578])|(1[02]))-31)))" 
+                            value="" required="" >
                         </div>
                     </div>
                     
