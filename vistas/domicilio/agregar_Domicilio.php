e0e2a3dbe216dace908b3845ac78f9bf7c2546bf
gosmithb <bryangodinez00@gmail.com>
2023-04-05 11:07:02 -0700

    Correccion en vista

--- vistas/domicilio/agregar_Domicilio.php
+++ vistas/domicilio/agregar_Domicilio.php
@@ -37,7 +37,7 @@
                 <div class="form-group">
                   <label class="control-label col-md-3" for="id_Cliente">Nombre del Cliente *</label>
                   <div class="col-md-8">
-                    <select class="form-control" id="idc" name="id_Cliente" method="post" type="text" placeholder="Selecciona el nombre del cliente" onchange="toggleButtonagregarDomicilio();toggleListadeclientes()">
+                    <select class="form-control" id="idc" name="id_Cliente" method="post" type="text" placeholder="Selecciona el nombre del cliente" onchange="handleSubmit();toggleListadeclientes()">
                       <option value disabled>Seleccione un cliente</option>
                       <?php foreach ($this->modelo->ListarClientes() as $tallerSQL) : ?>
                         <option id="<?= $tallerSQL->idClientes ?>" value="<?= $tallerSQL->idClientes ?>"><?= $tallerSQL->nombreCliente, " ", $tallerSQL->apellidoP . " ", $tallerSQL->apellidoM ?></option>
@@ -87,8 +87,8 @@
                 <div class="form-group">
                   <label class="control-label col-md-3" for="Problematica">Problematica para el servicio *</label>
                   <div class="col-md-8">
-                    <textarea class="form-control" id="obs" name="problematica" rows="4" placeholder="Problematica para el servicio" onkeydown="toggleButtonagregarDomicilio()"></textarea>
-                    <div class="alert alert-danger" role="alert" id="advertencia-problematica" hidden>
+                    <textarea class="form-control" id="obs" name="problematica" rows="4" placeholder="Problematica para el servicio" onkeyup="handleSubmit()"></textarea>
+                    <div class="alert alert-danger" role="alert" id="advertenciaProblematica" hidden>
                       Campo obligatorio
                     </div>
                   </div>
@@ -96,25 +96,25 @@
                 <!-- Todo esto es necesario? -->
                 <div class="form-group">
                   <div>
-                    <input class="form-control" name="observaciones" pattern="[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ. ]{7,100}" type="hidden">
+                    <input class="form-control" name="observaciones" type="hidden">
                   </div>
                 </div>
 
                 <div class="form-group">
                   <div>
-                    <input class="form-control" name="presupuesto" pattern="[0-9.]{1,12}" type="hidden">
+                    <input class="form-control" name="presupuesto" type="hidden">
                   </div>
                 </div>
 
                 <div class="form-group">
                   <div>
-                    <input class="form-control" name="costoTotal" pattern="[0-9.]{1,12}" type="hidden">
+                    <input class="form-control" name="costoTotal" type="hidden">
                   </div>
                 </div>
                 <?php
                 date_default_timezone_set('America/Mazatlan');
                 $fecha_actual = date("Y-m-d");
-                $hora_actual = date("H:i");
+                $hora_actual = date("H:i:S");
                 ?>
                 <div>
                   <div>
@@ -124,20 +124,20 @@
 
                 <div class="form-group">
                   <div>
-                    <input class="form-control" name="horaFinal" type="hidden" value="">
+                    <input class="form-control" name="horaFinal" type="hidden">
                   </div>
                 </div>
 
                 <div class="form-group">
                   <div>
-                    <input class="form-control" name="horasRealizadas" type="hidden" placeholder="horas realizadas" pattern="[0-9.]{1,12}">
+                    <input class="form-control" name="horasRealizadas" type="hidden" placeholder="horas realizadas">
                   </div>
                 </div>
 
                 <div class="form-group">
                   <label class="control-label col-md-3" for="FechaProgramada">Fecha solicitud de servicio</label>
                   <div class="col-md-8">
-                    <input class="form-control" id="fecha" name="fechaProgramada" type="date" min="<?= $fechadiadeHOY ?>" max="2030-01-01" placeholder="Fecha de solicitud de servicio" value="" onchange="toggleButtonagregarDomicilio()">
+                    <input class="form-control" id="fecha" name="fechaProgramada" type="date" min="<?= $fechadiadeHOY ?>" max="2030-01-01" placeholder="Fecha de solicitud de servicio" value="" onchange="handleSubmit()">
                   </div>
                 </div>
                 <div>
@@ -160,68 +160,12 @@
 
           <div class="col-lg-10 col-lg-offset-2">
             <button class="btn btn-primary" type="submit" id="submitButton" disabled>Enviar</button>
-            <button class="btn btn-default" type="reset" onclick="bloquearBotonEnviar()">Limpiar</button>
-            <button class="btn btn-default" type="button" onclick="cancelarDomicilio()">Cancelar</button>
+            <button class="btn btn-default" type="reset" onclick="handleBloquearSubmit()">Limpiar</button>
+            <button class="btn btn-default" type="button" onclick="handleCancelar()">Cancelar</button>
           </div>
         </div>
       </div>
     </div>
   </div>
 </div>
-<script>
-  let patrones = {
-    problematica: /[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ. ]{3,100}/
-  }
-
-  //este creo q no se usa
-  function toggleButtonagregarDomicilio() {
-    var idc = document.getElementById('idc').value;
-    var obs = patrones.problematica.test(document.getElementById('obs').value);
-    var fecha = document.getElementById('fecha').value;
-    let telefonoCliente = document.getElementById('listaTc').value;
-    let correoCliente = document.getElementById('listaCc').value;
-    let direccionCliente = document.getElementById('listaDc').value;
-
-    console.log(`idc: ${idc}`);
-    console.log(`obs: ${obs}`);
-    console.log(`fecha: ${fecha}`);
-    console.log(`telefono cliente: ${telefonoCliente}`);
-    console.log(`Correo cliente ${correoCliente}`);
-    console.log(`Direccion Cliente ${direccionCliente}`);
-
-
-    if (document.getElementById("obs").value != "") {
-      obs ?
-        document.getElementById('advertencia-problematica').hidden = true :
-        document.getElementById('advertencia-problematica').hidden = false
-    }
-
-    (obs && (fecha != "") && ((idc && telefonoCliente && correoCliente && direccionCliente) >= 0)) ?
-    document.getElementById('submitButton').disabled = false:
-      bloquearBotonEnviar();
-  }
-
-  //Funcion para poner en automatico los datos del cliente seleccionandolo de la lista
-  function toggleListadeclientes() {
-    document.getElementById('listaTc').value = document.getElementById('idc').value;
-    document.getElementById('listaCc').value = document.getElementById('idc').value;
-    document.getElementById('listaDc').value = document.getElementById('idc').value;
-  }
-
-  function bloquearBotonEnviar() {
-    document.getElementById('submitButton').disabled = true;
-  }
-
-  function cancelarDomicilio() {
-    Swal.fire({
-      title: '¿Deseas regresar a la lista y deshacer el registro?',
-      showCancelButton: true,
-      confirmButtonText: 'Confirmar',
-      cancelButtonText: 'Cancelar'
-    }).then((result) => {
-      if (result.isConfirmed) {
-        window.location.href = '?c=domicilio';
-      }
-    })
-  }
-</script>
+<script src="vistas/domicilio/agregar_Domicilio.js"></script>
