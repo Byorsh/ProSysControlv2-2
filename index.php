c5aae927fc3caabf40907ef828f942bb9cca939f
Byorsh <106332217+Byorsh@users.noreply.github.com>
2023-05-20 20:52:28 -0700

    login actualizado

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- CSS-->
-  <link rel="stylesheet" type="text/css" href="assets/css/main.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/login.css">
   <title>Login</title>
 
   <script src="vistas/alertas/alertas.js"></script>
@@ -21,25 +21,32 @@
 </head>
 
 <body>
-  <section class="material-half-bg">
-    <div class="cover"></div>
-  </section>
-  <section class="login-content">
-    <div class="logo">
-      <h1>ProSysControl</h1>
-    </div>
-    <div class="login-box">
-      <!-- Redirecciona a la validacion del login validar.php -->
-      <br><br>
-      <form class="login-form" action="validar.php" method="post">
-        <h3 class="login-head"><i class="fa fa-lg fa-fw fa-user"></i> <label>Inicio de sesion</label></h3><br><br>
-        <div class="login-group"> <label>Usuario: </label> <input type="text" placeholder="Ingrese el usuario" name="usuario" id="usuario" onkeyup="handleSubmit()" maxlength="40" min="1" onkeypress="return ((event.charCode >= 65 && event.charCode <= 90) || (event.charCode == 32) || (event.charCode >= 97 && event.charCode <= 122) || (event.charCode >= 160 && event.charCode <= 165) || (event.charCode == 224) || (event.charCode == 181) || (event.charCode == 130) || (event.charCode == 233) || (event.charCode == 144) || (event.charCode == 214))"></div>
-        <p class="alert-danger" id="advertenciaUsuario" style="float: left;" hidden>
-          Min. de 3 caracteres, solo letras y espacios
-        </p>
-        <div class="login-group"><label>Contraseña:</label> <input type="password" placeholder="Ingrese su contraseña" id="password" onkeyup="handleSubmit()" maxlength="70" min="1" name="contraseña" onkeypress="return ((event.charCode >= 65 && event.charCode <= 90) || (event.charCode >= 97 && event.charCode <= 122) || (event.charCode >= 48 && event.charCode <= 57))"> </div>
-        <p class="login-group"><button type="submit" value="Ingresar" id="submitButton" disabled>Ingresar</button></p>
-      </form>
+  <div class="login-card">
+    <h1>ProSysControl</h1>
+    <h2>Iniciar sesion</h2>
 
-    </div>
-  </section>
+    <form class="login-form" action="validar.php" method="post">
+      <input type="text" placeholder="Usuario" name="usuario" id="usuario" onkeyup="handleSubmit()" maxlength="40" min="1" 
+      onkeypress="return ((event.charCode >= 65 && event.charCode <= 90) || (event.charCode == 32) || (event.charCode >= 97 && event.charCode <= 122) || (event.charCode >= 160 && event.charCode <= 165) || (event.charCode == 224) || (event.charCode == 181) || (event.charCode == 130) || (event.charCode == 233) || (event.charCode == 144) || (event.charCode == 214))"/>
+      <p class="alert-danger" id="advertenciaUsuario" style="float: left;" hidden>
+          Min. de 3 caracteres, solo letras y espacios
+      </p>
+      <input type="password" placeholder="Contraseña" id="password" onkeyup="handleSubmit()" maxlength="70" min="1" name="contraseña" 
+      onkeypress="return ((event.charCode >= 65 && event.charCode <= 90) || (event.charCode >= 97 && event.charCode <= 122) || (event.charCode >= 48 && event.charCode <= 57))"/>
+      <button type="submit" value="Ingresar" id="submitButton">Ingresar</button>
+    </form>
+  </div>
+  <script>
+    document.addEventListener("DOMContentLoaded", function(){
+      var loginButton = document.getElementById("submitButton");
+      var user = document.getElementById("usuario");
+      var passwood = document.getElementById("password");
+
+      password.addEventListener("keyup", function(event) {
+        if (event.keyCode === 13) {
+          event.preventDefault();
+          loginButton.click();
+        }
+      });
+    });
+  </script>
